2bffa52c083cef2ffca8063cd3855b6bccb86286
Daniel Ponce <dmpb019@gmail.com>
2019-08-13 10:57:03 -0500

    Agregar array a la variable

--- app/User.php
+++ app/User.php
@@ -33,7 +33,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $appends;
+    protected $appends = [];
 
     /**
      * The attributes that should be cast to native types.
